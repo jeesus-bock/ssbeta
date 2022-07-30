a79164ffd1d503aefc6db196d84892bb0a087b71
Jussi <jussi.pesonen@adaptek.fi>
2022-07-30 17:49:46 +0300

    Changed Routes to FileRoutes in root.tsx

--- src/root.tsx
+++ src/root.tsx
@@ -1,21 +1,21 @@
 // @refresh reload
-import { Links, Meta, Routes, Scripts } from "solid-start/root";
-import { ErrorBoundary } from "solid-start/error-boundary";
-import { Suspense } from "solid-js";
+import { Links, Meta, FileRoutes, Scripts } from 'solid-start/root';
+import { ErrorBoundary } from 'solid-start/error-boundary';
+import { Suspense } from 'solid-js';
 
 export default function Root() {
   return (
-    <html lang="en">
+    <html lang='en'>
       <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charset='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
         <Meta />
         <Links />
       </head>
       <body>
         <ErrorBoundary>
           <Suspense>
-            <Routes />
+            <FileRoutes />
           </Suspense>
         </ErrorBoundary>
         <Scripts />
